54fdd3b7ae5ef7c472d43c247377c7a113346927
utpal14526 <68875052+utpal14526@users.noreply.github.com>
2023-04-04 17:16:27 +0530

    Update db.js
    new trika for connect to db

--- db.js
+++ db.js
@@ -1,12 +1,20 @@
-const mongoose=require('mongoose');
-
+const mongoose = require('mongoose')
 
 const URI="mongodb://localhost:27017/inotebook";
 
-const connecttomongo=()=>{
-    mongoose.connect(URI,()=>{
-        console.log("Connected to Mongo");
-    })
+
+const connecttomongo = async () => {
+    try {
+        mongoose.set('strictQuery', false)
+        mongoose.connect(URI) 
+        console.log('Mongo connected')
+    } catch(error) {
+        console.log(error);
+    }
 }
 
-module.exports=connecttomongo;   
+module.exports = connecttomongo
+
+// updated version to connect with db
+
+    
